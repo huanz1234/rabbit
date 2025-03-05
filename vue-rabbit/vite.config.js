533d5d50172a7788c62c9c08484190f22757eb98
huan <huanzhang1832601@gmail.com>
2025-03-05 21:41:32 +0800

    axios

--- vue-rabbit/vite.config.js
+++ vue-rabbit/vite.config.js
@@ -4,10 +4,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      // 通知elmentui
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
+    }),
     vueDevTools(),
   ],
   resolve: {
@@ -15,4 +28,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
 })
